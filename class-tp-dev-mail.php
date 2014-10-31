7427e800d55bc6186f44759608cccf232856d20d
Harold Angenent <harold@trendwerk.nl>
2014-10-31 15:42:28 +0100

    Standalone plugin

---
+++ class-tp-dev-mail.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: Development mails
+ * Description: Redirect all mails on development and staging environments.
+ */
+
+class TP_Dev_Mail {
+	function __construct() {
+		if( ! defined( 'TP_ENV' ) || 'develop' == TP_ENV || 'release' == TP_ENV )
+			add_filter( 'wp_mail', array( $this, 'send_to' ) );
+	}
+
+	/**
+	 * Don't send e-mails in develop and release environments
+	 */
+	function send_to( $mail ) {
+		$mail['subject'] .= ' [' . $mail['to'] . ']';
+
+		if( 'release' == TP_ENV )
+			$mail['to'] = get_option( 'admin_email' );
+		else if( defined( 'TP_DEV_MAIL' ) )
+			$mail['to'] = TP_DEV_MAIL;
+		else
+			$mail['to'] = '';
+
+		return $mail;
+	}
+	
+} new TP_Dev_Mail;
